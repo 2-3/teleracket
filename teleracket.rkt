b12a5fcd8a775b4a15d334f7ff6db0cace987ec2
zwwwdr <zwdr@cock.li>
2017-02-06 14:25:59 +0100

    Improved interface and error handling

--- teleracket.rkt
+++ teleracket.rkt
@@ -1,6 +1,6 @@
 #lang racket
-(require net/http-client)
-(require json)
+(require net/http-client
+         json)
 
 (provide
  api-get-updates
@@ -9,7 +9,7 @@
  api-forward-message)
 
 ; see https://core.telegram.org/bots/api for information on how to get a token
-(define bot-token "")
+(define bot-token "289593631:AAExfFvE5GcjawA3wEiWARhzrt47Ey7ZpbQ")
 (define api-endpoint-host "api.telegram.org")
 (define api-endpoint-uri (string-append "/bot" bot-token "/"))
 
@@ -17,14 +17,17 @@
   (jsexpr->string options))
 
 (define (api-request method action (options '()))
-  (define-values (status header response)
+  (define-values (status header json-encoded-response)
     (http-sendrecv
      api-endpoint-host
      (string-append api-endpoint-uri action)
      #:ssl? #t
      #:data (make-request-options options)
      #:headers (list "Content-Type: application/json")))
-  (read-json response))
+  (define response read-json json-encoded-response)
+  (if (hash-ref response 'ok)
+    ('ok  . (hash-ref response 'result)
+     'err . ((hash-ref response 'description) (hash-ref response 'error_code)))))
 
 (define (api-get-request action (options '()))
   (api-request "GET" action options))
@@ -32,27 +35,34 @@
 (define (api-post-request action (options  '()))
   (api-request "POST" action options))
 
-(define (api-get-updates (offset 0) (limit 100) (timeout 0))
-  (let* ((options (hash 'offset offset 'limit limit 'timeout timeout))
-         (response (api-get-request "getUpdates" options))
-         (updates (hash-ref response 'result))
-         (next-offset (+ 1 (if (empty? updates)
-                         offset
-                         (hash-ref (last updates) 'update_id)))))
-    (values updates (make-get-updates-with-offset next-offset))))
+(define (api-get-updates (offset 0) (limit 100) (timeout 0) (allowed-updates '()))
+  (let* ((options (hash 'offset offset
+                        'limit limit
+                        'timeout timeout
+                        'allowed-updates allowed-updates))
+         (response (api-get-request "getUpdates" options))))
+  (values response
+          (make-get-updates-with-offset
+            (case (car response)))
+              ('ok  (+ 1 (hash-ref (last updates) 'update_id)))
+              ('err offset)))
 
 (define (api-get-botinfo)
-  (hash-ref (api-get-request "getMe") 'result))
+  (api-get-request "getMe"))
 
-(define (make-get-updates-with-offset offset)
-  (λ ((limit 100) (timeout 0)) (api-get-updates offset limit timeout)))
+(define (make-get-updates-with-offset offset (default-limit 100) (default-timeout 0) (default-allowed-updates))
+  (λ ((limit default-limit) (timeout default-timeout) (allowed-updates default-allowed-updates))
+     (api-get-updates offset limit timeout allowed-updates)))
 
 (define (api-send-message chat-id text)
-  (let* ((message-hash (hash 'chat_id chat-id 'text text))
+  (let* ((message-hash (hash 'chat_id chat-id
+                             'text text))
          (response (api-post-request "sendMessage" message-hash)))
     response))
 
 (define (api-forward-message to-chat-id from-chat-id message-id)
-  (let* ((message-hash (hash 'chat_id to-chat-id 'from_chat_id from-chat-id 'message_id message-id))
+  (let* ((message-hash (hash 'chat_id to-chat-id
+                             'from_chat_id from-chat-id
+                             'message_id message-id))
          (response (api-post-request "forwardMessage" message-hash)))
     response))
